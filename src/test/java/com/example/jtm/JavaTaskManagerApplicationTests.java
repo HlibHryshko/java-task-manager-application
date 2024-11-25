c5e0bb3c673a5acf153cbe006abbe57d91830984
Hlib Hryshko <glebg27@gmail.com>
2024-11-25 18:13:13 +0100

    Initial commit

---
+++ src/test/java/com/example/jtm/JavaTaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jtm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
