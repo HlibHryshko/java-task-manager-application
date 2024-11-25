fb2d11e229aea2f25e3b1b04a0421f3eebbc1f6e
Hlib Hryshko <glebg27@gmail.com>
2024-11-25 19:27:25 +0100

    task entity service is implemented with getAll, create and setAsComplete functionalities

---
+++ src/main/java/com/example/jtm/services/entity/TaskEntityService.java
@@ -0,0 +1,40 @@
+package com.example.jtm.services.entity;
+
+import com.example.jtm.models.entities.Task;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class TaskEntityService {
+    private final JpaRepository<Task, Long> repository;
+
+    public TaskEntityService(JpaRepository<Task, Long> repository) {
+        this.repository = repository;
+    }
+
+    @Transactional
+    public Set<Task> getAll() {
+        return new HashSet<>(repository.findAll());
+    }
+
+    @Transactional
+    public Task create(Task task) {
+        return repository.save(task);
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Transactional
+    public Task markAsCompleted(Long id) {
+        Task task = repository.findById(id).orElseThrow(() -> new RuntimeException("Task not found"));
+        task.setCompleted(true);
+        return repository.save(task);
+    }
+}
